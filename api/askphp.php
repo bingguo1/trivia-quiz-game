2386bd665980ca5a79c64d16699d596cbe6c6d99
Bing Guo <bingguo0625@gmail.com>
2026-03-27 21:40:37 -0400

    fix newline display issues

--- api/askphp.php
+++ api/askphp.php
@@ -49,7 +49,7 @@ try {
     $a = ["succeed"];
     while ($row = $stmt->fetch()) {
         foreach ($columnSets as $column) {
-            array_push($a, nl2br($row[$column]));
+            array_push($a, $row[$column]);
         }
     }
     echo json_encode($a);
